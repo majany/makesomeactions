ae0de2b84674d01be95ab0761e1930307878bb6d
majany <mati94@arcor.de>
2025-03-03 21:35:15 +0100

    Update provisionscript_fp16_flux.sh

--- provisionscript_fp16_flux.sh
+++ provisionscript_fp16_flux.sh
@@ -33,7 +33,6 @@ CHECKPOINT_MODELS=(
 DIFFUSION_MODELS=(
     #"https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/diffusion_models/wan2.1_i2v_720p_14B_fp8_e4m3fn.safetensors"
     "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/diffusion_models/wan2.1_i2v_720p_14B_bf16.safetensors"
-    ""
 )
 
 CLIP_VISION=(
