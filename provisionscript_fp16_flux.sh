14ffa704c12c3b0b904109e40ea1868e2f4752f9
majany <mati94@arcor.de>
2025-03-04 18:17:37 +0100

    Update provisionscript_fp16_flux.sh

--- provisionscript_fp16_flux.sh
+++ provisionscript_fp16_flux.sh
@@ -26,7 +26,7 @@ WORKFLOWS=(
 
 CHECKPOINT_MODELS=(
     #"https://civitai.com/api/download/models/798204?type=Model&format=SafeTensor&size=full&fp=fp16"
-    "https://civitai.com/api/download/models/1413133?type=Model&format=SafeTensor&size=full&fp=fp16"
+    #"https://civitai.com/api/download/models/1413133?type=Model&format=SafeTensor&size=full&fp=fp16"
     "https://civitai.com/api/download/models/1031531?type=Model&format=SafeTensor&size=full&fp=fp8"
 )
 
