8bd5f3b7de25ea6e2591ccda4a0ed6a66ca20f1d
Mathaeus Jany <majany94@gmail.com>
2025-03-07 20:01:21 +0100

    remove useless model + replace civit token automatically

--- ownprovisionscript.sh
+++ ownprovisionscript.sh
@@ -4,4 +4,6 @@ sudo apt install aria2
 
 wget https://raw.githubusercontent.com/majany/makesomeactions/refs/heads/main/modelstodownload.txt
 
+sed -i "s|%civitai_token%|${CIVITAI_TOKEN}|g" modelstodownload.txt
+
 aria2c -i modelstodownload.txt -j2 -x 16 -s 16
